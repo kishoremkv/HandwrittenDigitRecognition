7d0896e0dc0dd41e5616e680a0564fc71ecaedd7
adityajain17 <adtjn9@gmail.com>
2020-05-29 22:00:06 +0530

    make it better

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/s2",function(req,res)
     dataToSend = data.toString();
     });
     python.stderr.on('data',(data)=>{
-        console.log(Uint8Array.toString(data));
+        console.log("The Error is "+data.toString());
     });
     // in close event we are sure that stream from child process is closed
     python.on('close', (code) => {
