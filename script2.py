deed06bf403d1032cb7d77d29e48ce720f5a6f99
M K V KISHORE <krishnaveerakishoremuppana@gmail.com>
2020-05-29 22:32:23 +0530

    changed script2

--- script2.py
+++ script2.py
@@ -1,6 +1,12 @@
 import sys
 import numpy as np
 import pandas as pd
+import subprocess
+import sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+package = "-r requirements.txt"
+install(package)
 print('#Hello from python#')
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
